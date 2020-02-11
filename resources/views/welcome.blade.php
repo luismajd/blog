c1aceba16932d7ea69f22cc73472a186dc65eee5
Luis Juarez <luis_majd@hotmail.com>
2020-02-11 16:23:06 -0600

    Cambio de titulo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Blog de prueba</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Mi blog de prueba
                 </div>
 
                 <div class="links">
